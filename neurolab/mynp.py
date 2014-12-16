72c42f23317c5c8d511a3a7139bc01daca493571
inferrna <inferrna@gmail.com>
2014-12-16 18:53:44 +0800

    +mynp

---
+++ neurolab/mynp.py
@@ -0,0 +1,126 @@
+import numpy as np
+
+
+def argmin(*args, **kwargs):
+    return np.argmin(args, **kwargs)
+
+
+def concatenate(*args, **kwargs):
+    return np.concatenate(args, **kwargs)
+
+
+def dot(*args, **kwargs):
+    return np.dot(args, **kwargs)
+
+
+def floor(*args, **kwargs):
+    return np.floor(args, **kwargs)
+
+
+def isneginf(*args, **kwargs):
+    return np.isneginf(args, **kwargs)
+
+
+def ones_like(*args, **kwargs):
+    return np.ones_like(args, **kwargs)
+
+
+def row_stack(*args, **kwargs):
+    return np.row_stack(args, **kwargs)
+
+
+def tanh(*args, **kwargs):
+    return np.tanh(args, **kwargs)
+
+
+def all(*args, **kwargs):
+    return np.all(args, **kwargs)
+
+
+def asfarry(*args, **kwargs):
+    return np.asfarry(args, **kwargs)
+
+
+def exp(*args, **kwargs):
+    return np.exp(args, **kwargs)
+
+
+def linspace(*args, **kwargs):
+    return np.linspace(args, **kwargs)
+
+
+def min(*args, **kwargs):
+    return np.min(args, **kwargs)
+
+
+def sqrt(*args, **kwargs):
+    return np.sqrt(args, **kwargs)
+
+
+def values(*args, **kwargs):
+    return np.values(args, **kwargs)
+
+
+def isinf(*args, **kwargs):
+    return np.isinf(args, **kwargs)
+
+
+def items(*args, **kwargs):
+    return np.items(args, **kwargs)
+
+
+def max(*args, **kwargs):
+    return np.max(args, **kwargs)
+
+
+def py(*args, **kwargs):
+    return np.py(args, **kwargs)
+
+
+def abs(*args, **kwargs):
+    return np.abs(args, **kwargs)
+
+
+def empty(*args, **kwargs):
+    return np.empty(args, **kwargs)
+
+
+def random(*args, **kwargs):
+    return np.random(args, **kwargs)
+
+
+def argmax(*args, **kwargs):
+    return np.argmax(args, **kwargs)
+
+
+def square(*args, **kwargs):
+    return np.square(args, **kwargs)
+
+
+def sign(*args, **kwargs):
+    return np.sign(args, **kwargs)
+
+
+def zeros_like(*args, **kwargs):
+    return np.zeros_like(args, **kwargs)
+
+
+def sum(*args, **kwargs):
+    return np.sum(args, **kwargs)
+
+
+def zeros(*args, **kwargs):
+    return np.zeros(args, **kwargs)
+
+
+def Inf(*args, **kwargs):
+    return np.Inf(args, **kwargs)
+
+
+def asfarray(*args, **kwargs):
+    return np.asfarray(args, **kwargs)
+
+
+def array(*args, **kwargs):
+    return np.array(args, **kwargs)
+

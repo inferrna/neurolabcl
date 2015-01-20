31bf1672a354755a678f371d639ee8b03ece3baa
inferrna <inferrna@gmail.com>
2015-01-20 15:21:46 +0800

    ++

--- slicetest.py
+++ slicetest.py
@@ -1,17 +1,19 @@
 import numpy as np
 
-def mislice(gid, N, d, x, y):
+def mislice(gid, params):
+    N, d, x, y = params[-1]
     ipg = 1+(min(N, y)-(x%N)-1)//d  #Items per group
     print("Items per group is", ipg)
     s = x//N                        #Group shift
     print("Shift is", s)
     group = s+gid//ipg              #Current group
+    if len(params)>1: group = mislice(group, params[:-1])
     print("Current group is", group)
     groupstart = group*N            #Start index of group
     print("Start index of group is", groupstart)
     cmd = gid%ipg                   #Current modulo
     print("Current modulo is", cmd)
-    groupid = x%N+cmd*d             #Index of modulo in current grop
+    groupid = x%N+cmd*d             #Index of modulo in current group
     print("Index of modulo in current grop is", groupid)
     return  groupid+groupstart      #Index in all array
 
@@ -22,5 +24,5 @@ x%N+(gid%ipg)*d + (s+gid//ipg)*N
 x%N+(gid%ipg)*d + (x//N+gid//ipg)*N
 """
 
-arr = np.random.randint(55, size=(4, 9))
-rarr = arr.reshape((4*9,))
+arr = np.random.randint(55, size=(5, 7, 8))
+rarr = arr.reshape((arr.size,))

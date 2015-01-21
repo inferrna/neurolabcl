c11a0fd9a373b2050e73c2e670048cbec54a14c3
inferrna <inferrna@gmail.com>
2015-01-21 15:45:38 +0800

    +start slice 4gpu

--- slicetest.py
+++ slicetest.py
@@ -26,3 +26,28 @@ x%N+(gid%ipg)*d + (x//N+gid//ipg)*N
 
 arr = np.random.randint(55, size=(5, 7, 8))
 rarr = arr.reshape((arr.size,))
+
+
+"""
+#define dtype uint
+#define PC 3 //Dimensions count
+
+uint slice(uint id, uint4 *params){
+    uint N = params[id].s0;
+    uint d = params[id].s1;
+    uint x = params[id].s2;
+    uint y = params[id].s3;
+    uint ipg = 1+(min(N, y)-(x%N)-1)/d;
+    uint s = x//N;
+    uint group = s+gid/ipg;
+    for(uint i=id; i>=0; i--){
+
+    }
+
+
+}
+
+__kernel void mislice(__global uint4 *params, __global dtype *data){
+
+}
+"""

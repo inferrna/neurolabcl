1c5dad1f3be73b8b04667aed7b54ade3f658d6c0
inferrna <inferrna@gmail.com>
2015-01-27 16:48:37 +0800

    +fix in transpose prototype

--- transptest.py
+++ transptest.py
@@ -27,8 +27,8 @@ __kernel void mitransp(__global uint *olddims, __global uint *replaces, __global
     findpos(gid, olddims, currposs, PC-1);
     for(i=0; i<PC; i++){ //i as current dim
         j = replaces[i];
-        newposs[j] = currposs[i];
-        newdims[j] = olddims[i];
+        newposs[i] = currposs[j];
+        newdims[i] = olddims[j];
     }
     scales[PC-1] = 1;
     for(i=PC-1; i>0; i--){ //i as current dim
@@ -49,7 +49,7 @@ arr = np.random.randint(55, size=(3, 4, 5)).astype(np.uint32)
 rarr = arr.reshape((arr.size,))
 
 run = cl.Program(ctx, gpusrc).build()
-replaces = np.array([1,0,2], dtype=np.uint32)
+replaces = np.array([1,2,0], dtype=np.uint32)
 olddims = np.array(arr.shape, dtype=np.uint32)
 #params = np.array([[8, 2, 2, 3], [7, 3, 1, 4], [5, 2, 0, 5]], dtype=np.uint32)
 transposed = arr.transpose(replaces)

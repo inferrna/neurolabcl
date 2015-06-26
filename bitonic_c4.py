42a088a13d8a3745b64a49270baadfd021541cc6
inferrna <inferrna@gmail.com>
2015-06-26 16:59:30 +0800

    +working bitonic

--- bitonic_c4.py
+++ bitonic_c4.py
@@ -2,9 +2,10 @@ import mynp as np
 tplsrc = open("SortKernels.cl", "r").read()
 
 singleprogram = np.cl.Program(np.ctx, tplsrc).build()
-localmem = np.cl.LocalMemory(1024*4)
-arr = np.random.randn(1024)
-out = np.empty(1024, dtype=arr.dtype)
+sz = 512
+localmem = np.cl.LocalMemory(sz*4)
+arr = np.random.randn(sz)
+out = np.empty(sz, dtype=arr.dtype)
 arrc = arr.copy()
 print(arr)
 arrs = np.np.sort(arr.get())
@@ -35,54 +36,51 @@ def sort_c4(arr):
             strategy.append(d);
             ii >>= d
             print("ii =", ii)
+        while inc > 0:
+            ninc = 0;
+            kid = -1;
+            doLocal = 0;
+            nThreads = 0;
+            d = strategy.pop(0);
+
+            if d == -1:
+                kid = singleprogram.ParallelBitonic_C4 #PARALLEL_BITONIC_C4_KERNEL;
+                ninc = -1; # reduce all bits
+                doLocal = 4;
+                nThreads = n >> 2;
+            elif d == 4:
+                kid = singleprogram.ParallelBitonic_B16 #PARALLEL_BITONIC_B16_KERNEL;
+                ninc = 4;
+                nThreads = n >> ninc;
+            elif d == 3:
+                kid = singleprogram.ParallelBitonic_B8 #PARALLEL_BITONIC_B8_KERNEL;
+                ninc = 3;
+                nThreads = n >> ninc;
+            elif d == 2:
+                kid = singleprogram.ParallelBitonic_B4 #PARALLEL_BITONIC_B4_KERNEL;
+                ninc = 2;
+                nThreads = n >> ninc;
+            elif d == 1:
+                kid = singleprogram.ParallelBitonic_B2 #PARALLEL_BITONIC_B2_KERNEL;
+                ninc = 1;
+                nThreads = n >> ninc;
+            else:
+                print("Strategy error!");
+            wg = np.ctx.devices[0].max_work_group_size
+            wg = min(wg,256);
+            wg = min(wg,nThreads);
+            if doLocal>0:
+                localmem = np.cl.LocalMemory(wg*doLocal*arr.dtype.itemsize)
+                kid(np.queue, (nThreads,), (wg,), arr.data, np.np.int32(inc), np.np.int32(length<<1), localmem)
+            else: 
+                kid(np.queue, (nThreads,), (wg,), arr.data, np.np.int32(inc), np.np.int32(length<<1))
+            np.cl.enqueue_barrier(np.queue)
+            #c->enqueueBarrier(targetDevice); // sync
+            # if (mLastN != n) printf("LENGTH=%d INC=%d KID=%d\n",length,inc,kid); // DEBUG
+            if ninc < 0: break
+            inc >>= ninc
         length<<=1
         print("length =", length)
-    while inc > 0:
-        ninc = 0;
-        kid = -1;
-        doLocal = 0;
-        nThreads = 0;
-        d = strategy.pop(0);
-
-        if d == -1:
-          kid = singleprogram.ParallelBitonic_C4 #PARALLEL_BITONIC_C4_KERNEL;
-          ninc = -1; # reduce all bits
-          doLocal = 4;
-          nThreads = n >> 2;
-        elif d == 4:
-          kid = singleprogram.ParallelBitonic_B16 #PARALLEL_BITONIC_B16_KERNEL;
-          ninc = 4;
-          nThreads = n >> ninc;
-        elif d == 3:
-          kid = singleprogram.ParallelBitonic_B8 #PARALLEL_BITONIC_B8_KERNEL;
-          ninc = 3;
-          nThreads = n >> ninc;
-        elif d == 2:
-          kid = singleprogram.ParallelBitonic_B4 #PARALLEL_BITONIC_B4_KERNEL;
-          ninc = 2;
-          nThreads = n >> ninc;
-        elif d == 1:
-          kid = singleprogram.ParallelBitonic_B2 #PARALLEL_BITONIC_B2_KERNEL;
-          ninc = 1;
-          nThreads = n >> ninc;
-        else:
-          print("Strategy error!");
-        wg = np.ctx.devices[0].max_work_group_size
-        wg = min(wg,256);
-        wg = min(wg,nThreads);
-        #c->clearArgs(kid);
-        #c->pushArg(kid,out);
-        #c->pushArg(kid,inc); // INC passed to kernel
-        #c->pushArg(kid,length<<1); // DIR passed to kernel
-        localmem = np.cl.LocalMemory(wg*doLocal*arr.dtype.itemsize)
-        #c->enqueueKernel(targetDevice,kid,nThreads,1,wg,1,EventVector());
-        if doLocal>0: kid(np.queue, (nThreads,), (wg,), arr.data, np.np.int32(inc), np.np.int32(length<<1), localmem)
-        else: kid(np.queue, (nThreads,), (wg,), arr.data, np.np.int32(inc), np.np.int32(length<<1))
-        np.cl.enqueue_barrier(np.queue)
-        #c->enqueueBarrier(targetDevice); // sync
-        # if (mLastN != n) printf("LENGTH=%d INC=%d KID=%d\n",length,inc,kid); // DEBUG
-        if ninc < 0: break
-        inc >>= ninc
 
         
 sort_c4(arr)

dd059fb917fd00ad9bb31682f17327814ec52dbf
inferrna <inferrna@gmail.com>
2015-06-29 13:28:51 +0800

    +fix +going class

--- bitonic_c4.py
+++ bitonic_c4.py
@@ -1,27 +1,30 @@
 import mynp as np
-tplsrc = open("SortKernels.cl", "r").read()
+import time
+
+tplsrc = open("SortKernels.cl.tpl", "r").read()
 
 singleprogram = np.cl.Program(np.ctx, tplsrc).build()
-sz = 512
-localmem = np.cl.LocalMemory(sz*4)
+sz = pow(2, 25)
 arr = np.random.randn(sz)
 out = np.empty(sz, dtype=arr.dtype)
-arrc = arr.copy()
-print(arr)
-arrs = np.np.sort(arr.get())
+arrc = arr.get()
+
+tsc = time.time()
+arrs = np.np.sort(arrc)
+tec = time.time()
 
 def sort_c4(arr):
     n = arr.size
-    allowb4  = False 
-    allowb8  = False 
-    allowb16 = False 
+    allowb4  = True
+    allowb8  = True
+    allowb16 = True 
     length = 1
     while length<n:
         inc = length
         strategy = []
         ii = inc
         while ii > 0:
-            if ii in [126, 32, 8]:
+            if ii in [128, 32, 8]:
                 strategy.append(-1)
                 break
             d = 1
@@ -35,7 +38,6 @@ def sort_c4(arr):
             else: d = 1;
             strategy.append(d);
             ii >>= d
-            print("ii =", ii)
         while inc > 0:
             ninc = 0;
             kid = -1;
@@ -66,6 +68,7 @@ def sort_c4(arr):
                 nThreads = n >> ninc;
             else:
                 print("Strategy error!");
+            print("inc ==", inc)
             wg = np.ctx.devices[0].max_work_group_size
             wg = min(wg,256);
             wg = min(wg,nThreads);
@@ -82,8 +85,12 @@ def sort_c4(arr):
         length<<=1
         print("length =", length)
 
-        
+tsg = time.time()        
 sort_c4(arr)
+teg = time.time()
+
+print("Sorting {0} samples. Got {1} sec on CPU and {2} sec on GPU".format(sz, tec - tsc, teg - tsg))
+
 #singleprogram.ParallelBitonic_C4(np.queue, (arr.size,), None, arr.data, np.np.int32(32), np.np.int32(0), localmem)
 #singleprogram.ParallelMerge_Local(np.queue, (arr.size,), None, arr.data, out.data, localmem)
 
